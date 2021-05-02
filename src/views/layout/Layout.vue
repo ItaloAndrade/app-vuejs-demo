426c932381d5474df9cf8b49ad46cac5ce206dcf
italo.andrade <italo.andrade@b2usistemas.com.br>
2021-05-01 21:23:52 -0300

    Adição barra lateral !

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,21 +1,25 @@
 <template>
-<v-app>
-    <AppBar />
-    <Drawer />
-    <Content />
-</v-app>
+    <v-app>
+        <appBar />
+        <drawer />
+        <content-view />
+        <fab-thema />
+        <the-layout-fab />
+    </v-app>
 </template>
 
 <script>
-export default {
-    name: 'Layout',
-    components: {
-        AppBar: () => import('./AppBar'),
-        Drawer: () => import('./Drawer'),
-        Content: () => import('./Content'),
-    },
-    data: () => ({
-        expandOnHover: false,
-    }),
-}
+    export default {
+        name: 'Layout',
+        components: {
+            AppBar: () => import('./AppBar'),
+            Drawer: () => import('./Drawer'),
+            ContentView: () => import('./Content'),
+            FabThema: () => import('./FabThema'),
+            TheLayoutFab: () => import('./TheLayoutFab'),
+        },
+        data: () => ({
+            expandOnHover: false,
+        }),
+    }
 </script>
