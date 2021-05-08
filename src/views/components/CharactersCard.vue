7e69d22ca5a4c3f3727f9d77664beac0bf2a00ce
Italo Andrade <italorio@gmail.com>
2021-05-08 10:52:30 -0300

    🐛 adição analitic

--- src/views/components/CharactersCard.vue
+++ src/views/components/CharactersCard.vue
@@ -2,7 +2,7 @@
 <v-container justify="center" fluid>
     <v-row>
         <v-col @mouseover="show = i;" @mouseout="show = false;" v-for="(card, i) in getMarvel.characters" :key="i"   class="something-i-need-to-animate">
-            <v-card class="pa-3 elevation-3 mt-10">
+            <v-card class="pa-3 elevation-3 mt-10" max-width="250px">
                 <div class="d-flex grow flex-wrap ">
                     <v-img :src="card.thumbnail.path + '.' + card.thumbnail.extension" class="elevation-23 head-card white--text align-end" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="200px">
                     </v-img>
